36adc5be2b74b8f812518dd02925a2699fdeb985
Othman Alseagh <assaigh.2013@gmail.com>
2019-07-24 17:07:11 +0300

    changes in registration bage
    remove insert in course regester table, insert values to userhasrole table, crypt password and remove unusable code at bottom.

--- teacher_register.php
+++ teacher_register.php
@@ -1,7 +1,7 @@
 <?php
 include 'dbconnect.php';
 
-    if(!empty($name)&&!empty($email)&&!empty($password)&&!empty($re_password)&&!empty($phone)&&!empty($address)&&!empty($identity)&&!empty($birthdate)&&!empty($course)) {
+    if(!empty($name)&&!empty($email)&&!empty($password)&&!empty($re_password)&&!empty($phone)&&!empty($address)&&!empty($identity)&&!empty($birthdate)) {
         $name = mysqli_real_escape_string($connection, $name);
         $email = mysqli_real_escape_string($connection, $email);
         $password = mysqli_real_escape_string($connection, $password);
@@ -10,51 +10,26 @@ include 'dbconnect.php';
         $address = mysqli_real_escape_string($connection, $address);
         $identity = mysqli_real_escape_string($connection, $identity);
         $birthdate = mysqli_real_escape_string($connection, $birthdate);
-        $course = mysqli_real_escape_string($connection, $course);
-
-            $query = "INSERT INTO `users` (`userFullName`, `email`, `password`, `identityNO`, `phone`, `address`, `dateOfBirth`) VALUES ('{$name}', '{$email}', '{$password}', '{$identity}', '{$phone}', '{$address}', '{$birthdate}')";
-            mysqli_query($connection,$query);
-            $u_id = mysqli_insert_id($connection);
-            //print ($u_id);
-            $in_query = "INSERT INTO courseregester (userId,courseId) VALUES ('{$u_id}','{$course}'; INSERT INTO userhasrole (userId,roleId) VALUES ('{$u_id}','2')";
-            $re = mysqli_multi_query($connection,$in_query);
-            if(!$re){
-                die("error" . mysqli_error($connection));
-            }
+        //$course = mysqli_real_escape_string($connection, $course);
 
 
-    }
-//    $email = mysqli_real_escape_string($connection, $email);
-//    $password = mysqli_real_escape_string($connection, $password);
-//    mysqli_query($connect
-//ion,"SET NAMES 'utf8'");
-//    mysqli_query($connection,'SET CHARACTER SET utf8');
-//    $query = "SELECT * FROM users WHERE email = '{$email}'";
-//    $query_result = mysqli_query($connection, $query);
-//    if (!$query_result) {
-//        die("Error connection" . mysqli_connect_error($connection));
-//    }
-//    $result = mysqli_fetch_assoc($query_result);
-//    $user_id = $result['userId'];
-//    $user_name = $result['userFullName'];
-//    $user_email = $result['email'];
-//    $user_password = $result['password'];
-//    if ($email !== $user_email && $password !== $user_password) {
-//        header("Location: index.php");
+        mysqli_query($connection,"SET NAMES 'utf8'");
+        mysqli_query($connection,'SET CHARACTER SET utf8');
+        $password = crypt($password,'$2y$08$schoolmanagementothm13$');
+          $query = "INSERT INTO users (`userFullName`, `email`, `password`,`identityNO`, `phone`, `address`, `dateOfBirth`) VALUES ('{$name}', '{$email}','{$password}', '{$identity}', '{$phone}', '{$address}', '{$birthdate}')";
+             mysqli_query($connection,$query);
+             $u_id = mysqli_insert_id($connection);
+            $role_select = "SELECT roleId FROM roles WHERE roleName = 'teacher'";
+            $rol=mysqli_query($connection,$role_select);
+            $rol_sel=mysqli_fetch_assoc($rol);
+             $role_id=$rol_sel['roleId'];
+             $ins_query="INSERT INTO userhasrole (userId,roleId) VALUES ($u_id,$role_id)";
+            $que=mysqli_query($connection,$ins_query);
+             mysqli_query($connection,$que);
 //
-//    } else if ($email == $user_email && $password == $user_password) {
-//        $query_role= "SELECT * FROM users JOIN userhasrole ON users.userId=userhasrole.userId WHERE userhasrole.roleId = '{$user_id}'";
-//        $role_result = mysqli_query($connection,$query_role);
-//        $re=mysqli_fetch_assoc($role_result);
-//        if($re['roleId']== 1){
-//            header("Location: admin.php");
-//        }elseif ($re['roleId']== 2){
-//            header("Location: teacher.php");
-//        }else{
-//            header("Location: student.php");
-//        }
+//            $in_query = "INSERT INTO courseregester (userId,courseId) VALUES ('{$u_id}','{$course}') ";
+//            if (!$re = mysqli_multi_query($connection,$in_query)){
+//                die('errorend' . mysqli_error($connection));}
 //
-//    } else {
-//        header("Location: index.php");
-//    }
-
+//
+              }
