d7097744658f502fe1a3f543d85798c0c8a1b666
Othman Alseagh <assaigh.2013@gmail.com>
2019-07-24 17:03:09 +0300

    update teacher page
    remove the select COURSE option, insert view table to view all teacher`s info and remove footer include

--- teacher.php
+++ teacher.php
@@ -39,28 +39,7 @@
         <div class="site-mobile-menu-body"></div>
 
     </div>
-    <?php
-    if(isset($_POST['register'])){
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $re_password = $_POST['re_password'];
-        $phone = $_POST['phone'];
-        $address = $_POST['address'];
-        $identity = $_POST['identity'];
-        $birthdate = $_POST['birth_date'];
-        $course = $_POST['course'];
-        if(!empty($password && $re_password)) {
-            if ($password !== $re_password) {
-                print  $massage = "كلمة المرور غير متطابقة";
-            } else {
-                include "teacher_register.php";
-            }
-        }else{
-            header("location teacher.php");
-        }
-    }
-    ?>
+
 
     <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
 
@@ -73,8 +52,32 @@
                 $quary ="SELECT schoolName FROM settings";
                 $s_name = mysqli_query($connection,$quary);
                 $result = mysqli_fetch_assoc($s_name);
+                $school_name= $result['schoolName'];
+
+                if(isset($_POST['register'])){
+                    $name = $_POST['name'];
+                    $email = $_POST['email'];
+                    $password = $_POST['password'];
+                    $re_password = $_POST['re_password'];
+                    $phone = $_POST['phone'];
+                    $address = $_POST['address'];
+                    $identity = $_POST['identity'];
+                    $birthdate = $_POST['birth_date'];
+//                    $course = $_POST['course'];
+                    if(!empty($password && $re_password)) {
+                        if ($password !== $re_password) {
+                            print  $massage = "كلمة المرور غير متطابقة";
+                        } else {
+                            require "teacher_register.php";
+
+                        }
+                    }
+//                    else{
+//                        header("location: teacher.php");
+//                    }
+                }
                 ?>
-                <div class="site-logo mr-auto w-25"><a href="index.php"><?php  echo $result['schoolName']  ;  ?></a></div>
+                <div class="site-logo mr-auto w-25"><a href="index.php"><?php  echo $school_name  ;  ?></a></div>
                 <div class="mx-auto text-center">
                     <nav class="site-navigation position-relative text-right" role="navigation">
                         <ul class="site-menu main-menu js-clone-nav mx-auto d-none d-lg-block  m-0 p-0">
@@ -103,7 +106,7 @@
             <div class="container">
                 <div class="row align-item" >
                     <div class="row align-items-center" style="margin-left:400px !important" data-aos="fade-up" data-aos-delay="500">
-                        <form action="#" method="post" class="form-box">
+                        <form action="" method="post" class="form-box">
                             <h3 class="h4 text-black mb-4">تسجيل المدرس</h3>
                             <div class="form-group">
                                 <input type="text"  class="form-control" name="name"  placeholder="الاسم الكامل للمدرس" required>
@@ -119,19 +122,19 @@
                             </div>
                             <div class="form-group">
 
-                                <select name="course" required>
-<!--    it will bring the courses from database             -->
-                                    <option  selected="selected" value=""> اختر المادة</option>
-                                    <?php
-                                        $select=  "SELECT * FROM courses";
-                                        $courses= mysqli_query($connection,$select);
-                                       foreach ($courses as $course){
-                                            $name=$course['courseName'];
-                                        echo '<option onselect value="' . $course['courseId'] . '">' . $course['courseName'] . '</option>';
-                                        }
-                                    ?>
-                                </select>
-                                <?php mysqli_close($connection); ?>
+<!--                                <select name="course" required>-->
+<!--   it will bring the courses from database             -->
+<!--                                    <option  selected="selected" value=""> اختر المادة</option>-->
+<!--                                    --><?php
+//                                        $select=  "SELECT * FROM courses";
+//                                        $courses= mysqli_query($connection,$select);
+//                                       foreach ($courses as $course){
+//                                            $name=$course['courseName'];
+//                                        echo '<option onselect value="' . $course['courseId'] . '">' . $course['courseName'] . '</option>';
+//                                        }
+//                                    ?>
+<!--                                </select>-->
+
 <!--                                <input type=""  class="form-control" name="re-password" placeholder="أعد إدخال كلمة المرور" required>-->
                             </div>
                             <div class="form-group">
@@ -146,7 +149,7 @@
                                 <input type="number"  class="form-control" name="identity"  placeholder="رقم البطاقة" required>
                             </div>
                             <div class="form-group">
-                                <input type="submit" class="btn btn-primary btn-pill" name="register" value="تسجيل">
+                                <input type="submit" class="btn btn-primary btn-pill" name="register" value="تسجيل"  >
                             </div>
 <!--                            --><?php //echo $massage; ?>
                         </form>
@@ -157,10 +160,88 @@
         </div>
 
     </div>
+   
+    <footer class="footer-section bg-white">
+        <div class="container">
+            <div class="row">
+
+
+                <table >
+                    <tr>
+                        <th> </th>
+                        <th> </th>
+                        <th>عنوان الإقامة</th>
+                        <th>تاريخ الميلاد</th>
+                        <th>رقم الهاتف</th>
+                        <th>رقم البطاقة</th>
+<!--                        <th>كلمة المرور</th>-->
+                        <th>البريد الإلكتروني</th>
+                        <th>اسم المدرس</th>
+                    </tr>
+                    <?php
+                        mysqli_query($connection,"SET NAMES 'utf8'");
+                        mysqli_query($connection,'SET CHARACTER SET utf8');
+                        $table_select_query = "select * from users join userhasrole on users.userId=userhasrole.userId where userhasrole.roleId=2 ";
+                        $table_elements = mysqli_query($connection,$table_select_query);
+
+                    while ($element = mysqli_fetch_array($table_elements)){
+                        $u_id = $element['userId'];
+                        $u_name = $element['userFullName'];
+                        $u_email = $element['email'];
+                        $u_password = $element['password'];
+                        $u_identity = $element['identityNO'];
+                        $u_phone = $element['phone'];
+                        $u_address = $element['address'];
+                        $u_birth_date = $element['dateOfBirth'];
+
+
+                        echo'<tr>';
+                                echo '<form action="teacher_update.php" method="post">';
+                                echo '<th><button type="submit" name="button" class="btn-danger" value='.$u_id.'> حذف </button></th>';
+                                echo '<th><button type="submit" name="button" class="btn-success" value='.$u_id.'> تعديل </button></th>';
+                                echo '</form>';
+                                echo'<th>'. $u_address.'</th>';
+                                echo '<th>'. $u_birth_date.'</th>';
+                                echo'<th>'. $u_phone .'</th>';
+                                echo'<th>'. $u_identity.'</th>';
+//                                echo'<th>'. $u_password.'</th>';
+                                echo'<th>'. $u_email.'</th>';
+                                echo'<th>'. $u_name;'</th>';
+                        echo'</tr>';
+                     }
+                    ?>
+            </div>
+            <?php mysqli_close($connection); ?>
+            <div class="row pt-5 mt-5 text-center">
+                <div class="col-md-12">
+                    
+
+                </div>
+
+            </div>
+        </div>
+    </footer>
 
 
 
-    <?php include 'footer.php' ?>
+</div> <!-- .site-wrap -->
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-migrate-3.0.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/bootstrap-datepicker.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+
+
+<script src="js/main.js"></script>
 
 </body>
 </html>
